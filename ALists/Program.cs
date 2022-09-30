a25c1b349bf6b8230a1c1d36380bd06fbdb5a075
Matt Shirvan <56051637+mattshirvan@users.noreply.github.com>
2022-09-30 11:51:38 -0600

    Add files via upload

---
+++ ALists/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ALists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
